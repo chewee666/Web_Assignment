d9df17625b401d2a3981a41a0699851d0603bf92
chewee666 <chenjianyu112233@gmail.com>
2024-03-16 23:26:05 +0700

    My-Assignment1

---
+++ my-nuxt3/pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="bg-transparent p-6">
+      <div class="container mx-auto">
+        <div class="flex justify-center mb-8">
+          <input class="w-1/4 h-6 px-4 mb-4 mt-2 text-black text-center font-bold bg-transparent border-2 border-black rounded-md" type="search" placeholder="(Search)">
+        </div>
+        <div class="grid grid-cols-2 gap-20">
+          <div class="bg-transparent rounded-xl border-2 border-black overflow-hidden shadow-lg">
+            <img src="https://i.redd.it/w3kr4m2fi3111.png" alt="(Picture)" class="w-full h-48 object-contain">
+            <div class="p-4 flex items-center">
+            <div class="w-10 h-10 bg-transparent border-2 border-black rounded-full mr-4 flex-shrink-0"></div>
+            <p class="text-gray-700 text-base">Test Test Test Test Test Test Test Test.</p>
+            </div>
+          </div>
+  
+          <div class="bg-transparent rounded-xl border-2 border-black overflow-hidden shadow-lg">
+            <img src="https://i.redd.it/w3kr4m2fi3111.png" alt="(Picture)" class="w-full h-48 object-contain">
+            <div class="p-4 flex items-center">
+            <div class="w-10 h-10 bg-transparent border-2 border-black rounded-full mr-4 flex-shrink-0"></div>
+              <p class="text-gray-700 text-base">Test Test Test Test Test Test Test Test.</p>
+            </div>
+          </div>
+  
+          <div class="bg-transparent rounded-xl border-2 border-black overflow-hidden shadow-lg">
+            <img src="https://i.redd.it/w3kr4m2fi3111.png" alt="(Picture)" class="w-full h-48 object-contain">
+            <div class="p-4 flex items-center">
+            <div class="w-10 h-10 bg-transparent border-2 border-black rounded-full mr-4 flex-shrink-0"></div>
+              <p class="text-gray-700 text-base">Test Test Test Test Test Test Test Test.</p>
+            </div>
+          </div>
+  
+          <div class="bg-transparent rounded-xl border-2 border-black overflow-hidden shadow-lg">
+            <img src="https://i.redd.it/w3kr4m2fi3111.png" alt="(Picture)" class="w-full h-48 object-contain">
+            <div class="p-4 flex items-center">
+            <div class="w-10 h-10 bg-transparent border-2 border-black rounded-full mr-4 flex-shrink-0"></div>
+              <p class="text-gray-700 text-base">Test Test Test Test Test Test Test Test.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
+  <style scoped>
+input[type="search"]::placeholder {
+  color: black;
+}
+input[type="search"]:focus {
+  outline: none;
+}
+  </style>
+  
